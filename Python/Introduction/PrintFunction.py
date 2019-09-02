d21d5bfa85d26588b21fffe1e6d82a0e95a4cde0
Sathya pramod <sathyapramod12@gmail.com>
2019-09-02 18:27:16 +0530

    Add files via upload

---
+++ Python/Introduction/PrintFunction.py
@@ -0,0 +1,3 @@
+if __name__ == '__main__':
+    #n = int(input())
+    print(*range(1,int(input())+1),sep='')
---
+++ Python/Introduction/Writeafunction.py
@@ -0,0 +1,9 @@
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if (year % 4 != 0) or (year % 100 == 0 and year % 400 != 0):
+        leap = False
+    else:
+        leap = True
+    return leap
